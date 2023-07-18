0fe077a324663bccd3639ce8d1fa03606742d4eb
CamilaMandai <cayumandai@gmail.com>
2023-07-18 15:47:06 -0300

    fix: id of projects sections

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -47,7 +47,7 @@ export default function Projects() {
     },
   ]
   return (
-    <section className="project" id="project">
+    <section className="project" id="projects">
       <Container>
         <Row>
           <Col>
